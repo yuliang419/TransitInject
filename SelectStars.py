b143cbd5ce33605610e20b24f0ff312b587fb3d3
Liang Yu <yuliang@MIT.edu>
2017-02-07 12:56:26 -0500

    Bug fixes in SelectStars

--- SelectStars.py
+++ SelectStars.py
@@ -2,13 +2,19 @@ import numpy as np
 import pandas as pd
 import glob
 
-varcat = pd.read_table('varcat.txt', comment='#', header=0)
-variables = varcat[(varcat.Class != 'Noise ') & (varcat.Class != 'OTHPER')].values
+varcat = pd.read_table('varcat.txt', comment='#', header=0, skip_blank_lines=True)
+all_bad = varcat[(varcat.Class != 'Noise ') & (varcat.Class != 'OTHPER')].ID.values
+print 'No. of variables & EBs:', len(all_bad)
 
-all_stars = pd.read_csv('exofop_campaign1.csv', comment='\\', header=1)
-planets = all_stars[all_stars['Poss_pc'] == 'Y'].values
+camps = glob.glob('exofop*csv')
+for camp in camps:
+	all_stars = pd.read_csv(camp, comment='\\', header=0)
+	planets = all_stars[all_stars['Poss_pc'] == 'Y']
+	planets = planets['EPIC ID'].values
+	print 'No. of planet hosts:', len(planets)
+	all_bad = np.append(all_bad, planets)
 
-targlist = glob.glob('K2Campaign*targets.csv')
+targlist = glob.glob('k2/K2Campaign*targets.csv')
 mdwarf_props = 'GO1002|GO1006|GO1036|GO1037|GO1047|GO1050|GO1051|GO1053|GO1062|GO1075|GO2106|GO2073|GO2070|GO2069|GO2045|GO2031|GO2013|GO2011|\
 GO2011|GO3001|GO3011|GO301|GO3031|GO3034|GO3045|GO3069|GO3073|GO3094|GO3106|GO3107|GO3114'
 mdwarfs = np.array([])
@@ -16,6 +22,13 @@ for camp in targlist:
 	data = pd.read_csv(camp, header=0, index_col=False, usecols=[0,4], \
 		na_values='null')
 
-	md = data[data['Investigation IDs'].str.contains(mdwarf_props)].values
-	mdwarfs = np.append(mdwarfs, md)
+	md = data[data['Investigation IDs'].str.contains(mdwarf_props)]
+	good_md = md[~md['EPIC ID'].isin(all_bad)]
+	good_md = good_md['EPIC ID'].values.astype('str')
+
+	mdwarfs = np.append(mdwarfs, good_md)
+
+filenames = [mdwarfs[i]+'.txt' for i in range(len(mdwarfs))]
+np.savetxt('k2/k2mdwarfs/mdwarfs.ls', filenames, fmt='%10s')
+
 
